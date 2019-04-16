98e06b70478112c8bef5738f44d398dc59abc928
da.kolesnikov <da.kolesnikov@tensor.ru>
2019-04-17 01:06:22 +0500

    Подправил линтер

--- 2. Constructor/constructor.js
+++ 2. Constructor/constructor.js
@@ -17,13 +17,13 @@
  * @param {any} age 
  * @param {any} job 
  */
-function Constructor (name, age, job) {
+function Constructor(name, age, job) {
   this.name = name;
   this.age = age;
   this.job = job;
   this.sayName = function() {
     return 'My name is ' + this.name;
-  }
+  };
 }
 
 var instance1 = new Constructor('Dick', '5 years old', 'hardworker');
@@ -35,10 +35,10 @@ console.log(instance1.sayName());
 
 console.log(instance1.constructor == Constructor); // true
 
-console.log( instance1 instanceof Constructor); // true
-console.log( instance1 instanceof Object); // true
+console.log(instance1 instanceof Constructor); // true
+console.log(instance1 instanceof Object); // true
 
 var instance2 = new Constructor('Mick', '55 years old', 'softworker');
 
 // problem
-console.log(instance1.sayName == instance2.sayName) // false
+console.log(instance1.sayName == instance2.sayName); // false
