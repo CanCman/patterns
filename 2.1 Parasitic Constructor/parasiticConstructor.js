769dfc5553292e0c768c8f54d22907fb10f676f7
da.kolesnikov <da.kolesnikov@tensor.ru>
2019-05-10 04:02:46 +0500

    Добавил паттерн Паразитный конструктор

---
+++ 2.1 Parasitic Constructor/parasiticConstructor.js
@@ -0,0 +1,46 @@
+/**
+ * Преимущества:
+ * - можно добавить свой метод в контруктор;
+ * 
+ * Минусы:
+ * - получаемый объект не экземпляр своего конструктора;
+ * - функция контруктор не находится в ссылке constror объккта;
+ * - избыточность при создании методов для каждого экземпляра.
+ * 
+ * @param {any} name 
+ * @param {any} age 
+ * @param {any} job 
+ */
+function Obj(name, age, job) { 
+  var o = new Object();
+  o.name = name;
+  o.age = age;
+  o.job = job;
+
+  o.sayName = function() {
+    return 'My name is ' + this.name;
+  };
+
+  return o;
+}
+
+var instance1 = new Obj('Dick', '5 years old', 'hardworker');
+
+console.log(instance1.name); //Dick
+console.log(instance1.age); // 5 years old
+console.log(instance1.job); // hardworker
+console.log(instance1.sayName()); // My name is Dick
+
+console.log(instance1.constructor == Obj); // true
+
+console.log(instance1 instanceof Obj); // true
+console.log(instance1 instanceof Object); // true
+
+var instance2 = new Obj('Mick', '55 years old', 'softworker');
+
+console.log(instance2.name); // Mick
+console.log(instance2.age); // 55 years old
+console.log(instance2.job); // softworker
+console.log(instance2.sayName()); // My name is Mick
+
+console.log(instance1.sayName == instance2.sayName); // true
