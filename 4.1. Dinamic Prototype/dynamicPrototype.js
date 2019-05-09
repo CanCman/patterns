12833a0860bf39cc8c538ba576853d7b2cc1cd17
da.kolesnikov <da.kolesnikov@tensor.ru>
2019-05-10 04:03:31 +0500

    Добавил паттерн Динамический прототип

---
+++ 4.1. Dinamic Prototype/dynamicPrototype.js
@@ -0,0 +1,46 @@
+/**
+ * Преимущества:
+ * - инкапсулирует создание прототипа внутри объекта функции конструктора;
+ * 
+ * Минус:
+ * - разделяется код между конструктором и прототипом;
+ * 
+ * Особенности:
+ * - невозможно использовать нотацию присвоения объекта прототипу;
+ * 
+ * @param {any} name 
+ * @param {any} age 
+ * @param {any} job 
+ */
+function Obj(name, age, job) { 
+  this.name = name;
+  this.age = age;
+  this.job = job;
+
+  if (typeof this.sayName != 'function') {
+    Obj.prototype.sayName = function() {
+      return 'My name is ' + this.name;
+    };
+  }
+}
+
+var instance1 = new Obj('Dick', '5 years old', 'hardworker');
+
+console.log(instance1.name); //Dick
+console.log(instance1.age); // 5 years old
+console.log(instance1.job); // hardworker
+console.log(instance1.sayName()); // My name is Dick
+
+console.log(instance1.constructor == Obj); // true
+
+console.log(instance1 instanceof Obj); // true
+console.log(instance1 instanceof Object); // true
+
+var instance2 = new Obj('Mick', '55 years old', 'softworker');
+
+console.log(instance2.name); // Mick
+console.log(instance2.age); // 55 years old
+console.log(instance2.job); // softworker
+console.log(instance2.sayName()); // My name is Mick
+
+console.log(instance1.sayName == instance2.sayName); // true
